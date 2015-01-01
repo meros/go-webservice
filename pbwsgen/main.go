c4877fc108d098d4e1d4a76c61412cddc8166b4e
Alexander Schrab <alexander.schrab@gmail.com>
2015-01-01 19:01:29 +0100

    added some skeleton files for the generator

---
+++ pbwsgen/main.go
@@ -0,0 +1,6 @@
+// TODO: this will be an executable that takes a proto file, a (set of) req/resp pairs to use and outputs a number of ready to use stubs/libraries.
+
+package main
+
+func main() {
+}
