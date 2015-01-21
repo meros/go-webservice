1f56687714d9cb9ed8d46440f70306d1fa133688
Alexander Schrab <alexander.schrab@gmail.com>
2015-01-21 20:41:03 +0100

    Started on the generator

--- pbwsgen/main.go
+++ pbwsgen/main.go
@@ -1,6 +1,39 @@
-// TODO: this will be an executable that takes a proto file, a (set of) req/resp pairs to use and outputs a number of ready to use stubs/libraries.
-
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
+	protoFilePtr := flag.String(
+		"proto",
+		"protocol.proto",
+		"proto file containing req/resp definitions")
+	protoReqMessagePtr := flag.String(
+		"req",
+		"Req",
+		"req message")
+	protoRespMessagePtr := flag.String(
+		"resp",
+		"Resp",
+		"resp message")
+
+	// TODO: flags for:
+	// * generator type (go server, go client, js client)
+	// * project name
+
+	flag.Parse()
+
+	fmt.Println("Using", *protoFilePtr, "with req message:", *protoReqMessagePtr, "and resp message:", *protoRespMessagePtr)
+
+	// Make sure we have the file specified
+	_, err := os.Open(*protoFilePtr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// TODO: based on generator chosen, take appropriate steps to generate code
 }
